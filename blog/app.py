e17ad498b02b75d335c3fba8525f4cf3a15f771a
“Liuba <kundasl1@gmail.com>
2023-03-13 00:48:51 +0300

    L-4 added dockerfile, docker-compose.yaml, migrations

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
 
 from blog.views.users import users_app
 from blog.views.articles import articles_app
@@ -8,7 +9,8 @@ from blog.models.database import db
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///D:\\IT\\projects\\Flask\\sqlite.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+#/home/PycharmProjects/Flask/blog/
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
@@ -19,7 +21,7 @@ app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
 app.register_blueprint(auth_app, url_prefix="/auth")
 login_manager.init_app(app)
-
+migrate = Migrate(app, db, compare_type=True)
 @app.route("/")
 def index():
     return render_template("index.html")
